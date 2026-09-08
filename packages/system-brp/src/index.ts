63453e88f45edec05ee5595d0b3d7ffe3af365ec
Three-Wheeled Sloth <36046890+SlothMD@users.noreply.github.com>
2026-09-08 10:13:54 -0400

    feat: export BRP power-level rules

--- packages/system-brp/src/index.ts
+++ packages/system-brp/src/index.ts
@@ -2,4 +2,5 @@ export * from "./adapter.js";
 export * from "./characteristicGeneration.js";
 export * from "./firstSlice.js";
 export * from "./nativeCharacter.js";
+export * from "./powerLevel.js";
 export * from "./rulesSource.js";
