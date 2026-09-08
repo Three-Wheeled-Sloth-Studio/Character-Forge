dc957bcf1b7856520dfa92b0094aa2d4bfcf91bc
Three-Wheeled Sloth <36046890+SlothMD@users.noreply.github.com>
2026-09-08 09:12:33 -0400

    feat: export BRP characteristic generation

--- packages/system-brp/src/index.ts
+++ packages/system-brp/src/index.ts
@@ -1,4 +1,5 @@
 export * from "./adapter.js";
+export * from "./characteristicGeneration.js";
 export * from "./firstSlice.js";
 export * from "./nativeCharacter.js";
 export * from "./rulesSource.js";
