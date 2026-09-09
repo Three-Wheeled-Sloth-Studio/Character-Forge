77646fa35df7bffd5df8c504c0ec8131e86bcbf0
Three-Wheeled Sloth <36046890+SlothMD@users.noreply.github.com>
2026-09-09 13:42:01 -0400

    feat: export D&D guided narrative API

--- packages/system-dnd5e/src/index.ts
+++ packages/system-dnd5e/src/index.ts
@@ -9,6 +9,7 @@ export * from "./guidedCoreValidation.js";
 export * from "./guidedDefaults.js";
 export * from "./guidedFirstSlice.js";
 export * from "./guidedGenerate.js";
+export * from "./guidedNarrative.js";
 export * from "./guidedStandardArrayGenerate.js";
 export * from "./manualGenerate.js";
 export * from "./nameGeneration.js";
